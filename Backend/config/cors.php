20264ee9ede1f17d9879890883f8e6eb3df43248
Gael Alexander Basana Hernandez <basanaclash09@gmail.com>
2026-04-16 12:49:47 -0600

    Delete Backend/config/cors.php

--- Backend/config/cors.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
-    'allowed_headers' => ['*'],
-    'supports_credentials' => false,
-];
