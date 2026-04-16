1dff1786da6a8eacdcb4311c269dde580d6480f2
Gael Alexander Basana Hernandez <basanaclash09@gmail.com>
2026-04-16 13:05:53 -0600

    Update cors.php al dominio

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -3,7 +3,10 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+    'http://localhost:5173', 
+    'https://considerate-success-production-b16e.up.railway.app/' // Añade tu URL de producción aquí
+],
     'allowed_headers' => ['*'],
     'supports_credentials' => false,
 ];
