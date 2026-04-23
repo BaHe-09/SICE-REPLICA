07dc551acabe802affc0f1861b5a5a80cb459405
BaHe-09 <basanaclash09@gmail.com>
2026-04-23 12:18:14 -0600

    cambio en link

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -5,7 +5,7 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://localhost:5173', 
-        'https://considerate-success-production-b16e.up.railway.app' // <--- Sin la "/" al final
+        'https://sice.up.railway.app' // <--- Sin la "/" al final
     ],
     'allowed_headers' => ['*'],
     'supports_credentials' => false,
