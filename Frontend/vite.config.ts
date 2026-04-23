7cbb5768d3d3f00bedee39b7c07e21d7f903d90f
BaHe-09 <basanaclash09@gmail.com>
2026-04-23 12:34:36 -0600

    cambio en link en vite.config y mas completo

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,10 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+
+  // 👇 AGREGA ESTO
   preview: {
-    host: true,
-    allowedHosts: ['sice.up.railway.app']
-  }
+  host: true,
+  allowedHosts: [
+    'sice.up.railway.app/'
+  ]
+}
 })
